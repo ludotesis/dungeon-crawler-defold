672009374071fb219edfc99ba40cd067a8008ad1
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-05-29 14:51:50 -0300

    Modulo Mapa Generado (Gemini)

---
+++ Dungeon Crawler/modulos/mapa.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+-- 0 = Espacio vacío, 1 = Pared
+M.grid = {
+	{1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 1},
+	{1, 0, 1, 0, 1},
+	{1, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1}
+}
+
+-- Función para comprobar si una casilla es transitable
+function M.is_walkable(x, z)
+	if M.grid[x] and M.grid[x][z] == 0 then
+		return true
+	end
+	return false
+end
+
+return M
